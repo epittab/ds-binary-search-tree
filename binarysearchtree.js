76030d65bc659ef39f0ad520b687d89936aa6ca9
epittab <ep7m@me.com>
2020-08-14 20:09:09 -0400

    added dfs Preorder

--- binarysearchtree.js
+++ binarysearchtree.js
@@ -49,7 +49,7 @@ class BST{
     }
 
 
-    search(value) {
+    find(value) {
 
         let currentNode = this.root
         while (true) {
@@ -62,6 +62,56 @@ class BST{
         }
 
     }
+
+
+    bfs() {
+        //handle edge case
+        if (!this.root) return undefined;
+        //create queue and results array
+        let queue = []; 
+        let resultBFS = [];
+
+        queue.push(this.root);
+
+        while (queue.length  > 0 ) {
+            //dequeue current node
+            let variable = queue.shift();
+            //add to results array
+            resultBFS.push( variable.value );
+            // check for children nodes
+            if ( variable.left ) queue.push( variable.left )     
+            if ( variable.right ) queue.push( variable.right )
+        }
+        return resultBFS;
+    }
+
+    dfsPreorder() {
+        // handle edge case
+        if (!this.root) return undefined;
+        
+        // define varible to store return values
+        let visited = [];
+
+        //set current node to root
+        let currentNode = this.root;
+        
+        // define recursive helper function
+        function traverse(node) {
+            
+            visited.push(node.value);
+            if (node.left) traverse(node.left)
+            if (node.right) traverse(node.right)
+
+        }
+      
+        traverse(currentNode);
+        return visited;
+    }
+
+
+    //private
+
+    
 
 
 }
